437316d1b787733a7163b1deb648e43f67212f56
SpringSunYY <3217778348@qq.com>
2026-01-06 16:42:08 +0800

    排行表格

--- ruoyi-ui/src/views/movie/statistics/index.vue
+++ ruoyi-ui/src/views/movie/statistics/index.vue
@@ -1,63 +1,34 @@
 <template>
   <div class="dashboard-editor-container">
-
-    <panel-group @handleSetLineChartData="handleSetLineChartData"/>
-
     <el-row class="chart-wrapper">
       <BarRankingCharts @item-click="handleChartClick"/>
     </el-row>
     <el-row class="chart-wrapper">
       <BarRankingCharts direction="left" :interval-time="0" @item-click="handleChartClick"/>
     </el-row>
+    <el-row class="chart-wrapper">
+      <TableRanking  @rowClicked="handleChartClick"/>
+    </el-row>
   </div>
 </template>
 
 <script>
-import PanelGroup from '../../dashboard/PanelGroup'
-import LineChart from '../../dashboard/LineChart'
-import RaddarChart from '../../dashboard/RaddarChart'
-import PieChart from '../../dashboard/PieChart'
-import BarChart from '../../dashboard/BarChart'
-import BarRankingCharts from "@/components/echarts/BarRankingCharts.vue";
 
-const lineChartData = {
-  newVisitis: {
-    expectedData: [100, 120, 161, 134, 105, 160, 165],
-    actualData: [120, 82, 91, 154, 162, 140, 145]
-  },
-  messages: {
-    expectedData: [200, 192, 120, 144, 160, 130, 140],
-    actualData: [180, 160, 151, 106, 145, 150, 130]
-  },
-  purchases: {
-    expectedData: [80, 100, 121, 104, 105, 90, 100],
-    actualData: [120, 90, 100, 138, 142, 130, 130]
-  },
-  shoppings: {
-    expectedData: [130, 140, 141, 142, 145, 150, 160],
-    actualData: [120, 82, 91, 154, 162, 140, 130]
-  }
-}
+
+import BarRankingCharts from "@/components/echarts/BarRankingCharts.vue";
+import TableRanking from "@/components/echarts/TableRanking.vue";
 
 export default {
   name: 'Index',
   components: {
-    BarRankingCharts,
-    PanelGroup,
-    LineChart,
-    RaddarChart,
-    PieChart,
-    BarChart
+    TableRanking,
+    BarRankingCharts
   },
   data() {
     return {
-      lineChartData: lineChartData.newVisitis
     }
   },
   methods: {
-    handleSetLineChartData(type) {
-      this.lineChartData = lineChartData[type]
-    },
     handleChartClick(item) {
       console.log(item)
     }
